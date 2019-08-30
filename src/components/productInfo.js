767350d6705fe9329786754d9b8cac788eda102d
kedorafr <alexis@kedora.fr>
2019-08-30 11:35:23 +0200

    change design of images

--- src/components/productInfo.js
+++ src/components/productInfo.js
@@ -3,6 +3,7 @@ import React from 'react';
 const productInfo = ({ product }) => {
     return (
         <>
+            <hr/>
             <p className="has-text-weight-semibold is-size-2">{product.title}</p>
             <p className="has-text-left is-uppercase  is-size-5">{product.variants[0].title}</p>
             <p className=" has-text-right has-text-weight-bold is-size-3">${product.variants[0].price}</p>
