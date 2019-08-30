d25ace2cd24487695631a790d0408d2b4d63a89c
kedorafr <alexis@kedora.fr>
2019-08-30 14:09:09 +0200

    add notif

--- src/components/seo.js
+++ src/components/seo.js
@@ -70,8 +70,6 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     >
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"/>
-      <link href="https://teodosii.github.io/react-notifications-component/main.css" rel="stylesheet"/>
-
     </Helmet>
   )
 }
