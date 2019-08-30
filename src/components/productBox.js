9314e57737dd58a0c804d5efb73179301bd1a5db
kedorafr <alexis@kedora.fr>
2019-08-30 11:48:31 +0200

    edit filter

--- src/components/productBox.js
+++ src/components/productBox.js
@@ -12,12 +12,12 @@ const productBox = props => {
                     key={product.node.images[0].localFile.id}
                     alt={product.node.title}
                 />
-                <p className="has-text-weight-semibold">{product.node.title}</p>
+                <p className="has-text-weight-semibold has-text-black">{product.node.title}</p>
                 <div className="columns">
-                    <div className="column has-text-left is-uppercase">
+                    <div className="column has-text-left is-uppercase has-text-black">
                         {product.node.variants[0].title}
                     </div>
-                    <div className="column has-text-right has-text-weight-bold is-size-5">
+                    <div className="column has-text-right has-text-weight-bold is-size-5 has-text-black">
                         ${product.node.variants[0].price}
                     </div>
                 </div>
