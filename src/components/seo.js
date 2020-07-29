6487342f6271d587ed9bc662415bd0016cc81f02
alexislepresle <contact@alexislepresle.com>
2020-07-29 22:03:34 +0200

    improve performance

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
